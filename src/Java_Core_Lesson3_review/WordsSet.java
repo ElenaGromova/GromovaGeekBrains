48005edc9b1deda1d14aa50cc5d29468c5953979
ElenaGromova <g777elena@gmail.com>
2019-12-02 21:44:44 +0600

    2.12.2019 проба

---
+++ src/Java_Core_Lesson3_review/WordsSet.java
@@ -0,0 +1,34 @@
+package Java_Core_Lesson3_review;
+
+import java.util.*;
+
+class WordsSet {
+
+    private static List<String> initList(){
+        return Arrays.asList("A", "B", "C", "a", "b", "c", "D", "d", "E", "e", "A", "B", "C", "A", "B", "C", "D", "d", "E", "e");
+    }
+
+    private static Map<String, Integer> initMap(Collection<String> strings){
+        Map<String, Integer> result = new HashMap<>();
+        for (String s: strings) {
+            result.merge(s, 1, Integer::sum);
+        }
+        return result;
+    }
+
+    private static void getCountOfElements(Map<String, Integer> map) {
+        for (String s : map.keySet()) {
+            System.out.println("Элемент исходного массива " + s + " встречается " + map.get(s) + " разa");
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> wordsList = initList();
+        Map<String, Integer> wordsMap = initMap(wordsList);
+        Set<String> uniqWords = wordsMap.keySet();
+        System.out.println("Исходный массив слов: " + wordsList);
+        System.out.println("Уникальные элементы исходного массива слов: " + uniqWords);
+        getCountOfElements(wordsMap);
+        }
+    }
+
