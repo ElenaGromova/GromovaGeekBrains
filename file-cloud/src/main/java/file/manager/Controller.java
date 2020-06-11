5e2f43852c6c167cb17f4eacebaa1ecfa1fce54a
ElenaGromova <g777elena@gmail.com>
2020-06-11 14:03:16 +0600

    11.06 простой файловый менеджер

--- file-cloud/src/main/java/file/manager/Controller.java
+++ file-cloud/src/main/java/file/manager/Controller.java
@@ -4,9 +4,8 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -14,6 +13,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -25,9 +25,12 @@ public class Controller implements Initializable {
     ListView<FileInfo> fileList;
     @FXML
     TextField pathField;
-    Path root;
 
-    public void menuItenFileAction(ActionEvent actionEvent) {
+    Path root;
+    Path selectedCopyFile;
+    Path selectedMoveFile;
+
+    public void menuItemFileAction(ActionEvent actionEvent) {
         Platform.exit();
     }
 
@@ -51,6 +54,9 @@ public class Controller implements Initializable {
                             if (item.getLenght() == -1L){
                                 formattedFileLength = String.format("%-s", "[ DIR ]");
                             }
+                            if (item.getLenght() == -2L){
+                                formattedFileLength = "";
+                            }
                             String text = String.format("%s %-20s", formattedFilename, formattedFileLength);
                             setText(item.getFilename());
                         }
@@ -65,12 +71,16 @@ public class Controller implements Initializable {
         root = path;
         pathField.setText(root.toAbsolutePath().toString());
         fileList.getItems().clear();
+        fileList.getItems().add(new FileInfo(FileInfo.UP_TOKEN, -2L));
         fileList.getItems().addAll(scanFiles(path));
         fileList.getItems().sort(new Comparator<FileInfo>() {
             @Override
             public int compare(FileInfo o1, FileInfo o2) {
+                if (o1.getFilename().equals(FileInfo.UP_TOKEN)){
+                    return -1;
+                }
                 if ((int)Math.signum(o1.getLenght()) != (int)Math.signum(o2.getLenght())){
-
+                    return o1.getFilename().compareTo(o2.getFilename());
                 }
                 return new Long(o1.getLenght() - o2.getLenght()).intValue();
             }
@@ -95,4 +105,86 @@ public class Controller implements Initializable {
     }
 
 
+    public void fileListClicked(MouseEvent mouseEvent) {
+        if(mouseEvent.getClickCount() == 2){
+            FileInfo fileInfo = fileList.getSelectionModel().getSelectedItem();
+            if (fileInfo != null){
+                if (fileInfo.isDirectory()){
+                    Path pathTo = root.resolve(fileInfo.getFilename());
+                    goToPath(pathTo);
+                }
+                if (fileInfo.isUpElement()){
+                    Path pathTo = root.toAbsolutePath().getParent();
+                    goToPath(pathTo);
+                }
+            }
+
+        }
+    }
+
+    public void refresh(){
+        goToPath(root);
+    }
+
+    public void copyAction(ActionEvent actionEvent) {
+        FileInfo fileInfo = fileList.getSelectionModel().getSelectedItem();
+        if ( (selectedCopyFile == null) && (fileInfo == null || fileInfo.isUpElement() || fileInfo.isDirectory())){
+            return;
+        }
+        if (selectedCopyFile == null){
+            selectedCopyFile = root.resolve(fileInfo.getFilename());
+            ((Button) actionEvent.getSource()).setText("Coping: " + fileInfo.getFilename());
+            return;
+        }
+        if (selectedCopyFile != null){
+            try {
+                Files.copy(selectedCopyFile, root.resolve(selectedCopyFile.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                selectedCopyFile = null;
+                ((Button) actionEvent.getSource()).setText("Copy");
+                refresh();
+            }
+            catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно скопировать файл");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    public void moveAction(ActionEvent actionEvent) {
+        FileInfo fileInfo = fileList.getSelectionModel().getSelectedItem();
+        if ( (selectedMoveFile == null) && (fileInfo == null || fileInfo.isUpElement() || fileInfo.isDirectory())){
+            return;
+        }
+        if (selectedMoveFile == null){
+            selectedMoveFile = root.resolve(fileInfo.getFilename());
+            ((Button) actionEvent.getSource()).setText("Moving: " + fileInfo.getFilename());
+            return;
+        }
+        if (selectedMoveFile != null){
+            try {
+                Files.move(selectedMoveFile, root.resolve(selectedMoveFile.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                selectedMoveFile = null;
+                ((Button) actionEvent.getSource()).setText("Move");
+                refresh();
+            }
+            catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно переместить файл");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    public void deleteAction(ActionEvent actionEvent){
+        FileInfo fileInfo = fileList.getSelectionModel().getSelectedItem();
+        if (fileInfo == null || fileInfo.isUpElement() || fileInfo.isDirectory()){
+            return;
+        }
+        try {
+            Files.delete(root.resolve(fileInfo.getFilename()));
+        }
+        catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно удалить файл");
+            alert.showAndWait();
+        }
+    }
 }
--- file-cloud/src/main/java/file/manager/FileInfo.java
+++ file-cloud/src/main/java/file/manager/FileInfo.java
@@ -5,33 +5,48 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileInfo {
+    public static final String UP_TOKEN = "[..]";
+
     private String filename;
-    private  long lenght;
+    private  long length;
 
     public String getFilename() {
         return filename;
     }
 
     public long getLenght() {
-        return lenght;
+        return length;
     }
 
     public void setFilename(String filename) {
         this.filename = filename;
     }
 
-    public void setLenght(long lenght) {
-        this.lenght = lenght;
+    public void setLenght(long length) {
+        this.length = length;
+    }
+
+    public boolean isDirectory(){
+        return length == -1L;
+    }
+
+    public boolean isUpElement(){
+        return length == -2L;
+    }
+
+    public FileInfo(String filename, long length){
+        this.filename = filename;
+        this.length = length;
     }
 
     public FileInfo(Path path) {
         try {
             this.filename = path.getFileName().toString();
             if (Files.isDirectory(path)) {
-                this.lenght = -1L;
+                this.length = -1L;
             }
             else {
-                this.lenght = Files.size(path);
+                this.length = Files.size(path);
             }
         }
         catch (IOException e) {
