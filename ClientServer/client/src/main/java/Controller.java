37347765e353d29b3a369d6c08b5f1dc4bf4024f
ElenaGromova <g777elena@gmail.com>
2020-06-20 20:01:33 +0600

    20/06/2020 ---

--- ClientServer/client/src/main/java/Controller.java
+++ ClientServer/client/src/main/java/Controller.java
@@ -1,18 +1,20 @@
-package filemanager;
+package main.java;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class Controller {
     @FXML
@@ -92,36 +94,6 @@ public class Controller {
     }
 
     public void deleteAction(ActionEvent actionEvent) {
-        PanelController leftPC = (PanelController)leftPanel.getProperties().get("ctrl");
-        PanelController rightPC = (PanelController)rightPanel.getProperties().get("ctrl");
-
-        if(leftPC.getSelectedFileName() == null && rightPC.getSelectedFileName() == null){
-            Alert alert = new Alert(Alert.AlertType.ERROR, "File not choose", ButtonType.OK);
-            alert.showAndWait();
-            return;
-        }
-
-        PanelController srcPC = null, dstPC = null;
-        if (leftPC.getSelectedFileName() != null){
-            srcPC = leftPC;
-            dstPC = rightPC;
-        }
-        if (rightPC.getSelectedFileName() != null){
-            srcPC = rightPC;
-            dstPC = leftPC;
-        }
-
-        Path srcPath = Paths.get(srcPC.getCurrentPath(), srcPC.getSelectedFileName());
-        Path dstPath = Paths.get(dstPC.getCurrentPath()).resolve(srcPath.getFileName().toString());
-
-        try {
-            Files.delete(srcPath);
-            Files.delete(dstPath);
-            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
-        } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "File not delete", ButtonType.OK);
-            alert.showAndWait();
-        }
     }
 }
 
